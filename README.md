135c921a4a6895943a0ed7185af81f06bf41246e
Pierre Olivier Paquet <pierreolivierpaquet@hotmail.com>
2024-03-22 12:00:25 -0400

    ADDED [ errors.hpp ]

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ft_irc
+
+[PROJECT_NOTION](https://www.notion.so/FT-IRC-e700810e160b4ed4a61df50b4279db7f)
--- include/main.hpp
+++ include/main.hpp
@@ -7,10 +7,28 @@
 #ifndef		MAIN_HPP_
 # define	MAIN_HPP_
 
+# include	<vector>
+// # include	<list>
+// # include	<map>
+
 # include	"Network.hpp"
+# include	"errors.hpp"
 
 # ifndef	EXIT_SUCCESS
 #  define	EXIT_SUCCESS	0
 # endif	/*	EXIT_SUCCESS	*/
+
+///	----------------------------------------------------------- @section COLOR.S
+
+# define BLD_RED "\033[1;31m"			//	BOLD RED
+# define RED "\033[31m"					//	RED
+# define BLD_WHI "\033[1m"				//	BOLD WHITE
+# define WHI "\033[0m"					//	WHITE (reset color)
+# define BLD_GRE "\033[1;32m"			//	BOLD GREEN
+# define GRE "\033[32m"					//	GREEN
+# define BLD_YEL "\033[1;33m"			//	BOLD YELLOW
+# define YEL "\033[33m"					//	YELLOW
+# define BLD_ORA "\033[1;38;5;208m"		//	BOLS ORANGE
+# define ORA "\033[38;5;208m"			//	ORANGE
 
 #endif	/*	MAIN_HPP_	*/
