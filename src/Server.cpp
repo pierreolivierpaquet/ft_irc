554d5e4bc13b7264aeaf65234b6e23ec31dab886
niceguy <evalldore@gmail.com>
2024-03-28 01:17:47 -0400

    parsing

--- src/Server.cpp
+++ src/Server.cpp
@@ -4,7 +4,10 @@
 /*                                                                            */
 /******************************************************************************/
 
-#include	"Server.hpp"
+#include <cstring>
+#include <cstdlib>
+#include <sstream>
+#include "Server.hpp"
 
 /// @brief	Sets the static signal variable to false.
 bool Server::_sig = false;
@@ -26,7 +29,7 @@ void	Server::signalHandle( int num ) {
 }
 
 void	Server::setPort( std::string portnum ) {
-	int	set_port = std::atoi( portnum.c_str() );
+	int	set_port = atoi( portnum.c_str() );
 	if (set_port < 1024 || set_port > 49151 ) {
 		throw( std::runtime_error(BLD_RED ERR_MSG WHI INVALID_PORT) );
 	}
@@ -106,8 +109,6 @@ void Server::acceptNewClient( void ) {
 	t_pollfd newPoll;
 	socklen_t len = sizeof(clientAdd);
 
-	memset( &newClient, 0, sizeof(newClient));
-
 	int incofd = accept(_sock_fd, reinterpret_cast< struct sockaddr * >( &clientAdd) , &len);
 	if (incofd == -1) {
 		std::cout << "Accept() failed!" << std::endl;
@@ -128,6 +129,15 @@ void Server::acceptNewClient( void ) {
 	std::cout << "Client connected!" << std::endl;
 }
 
+static void splitInput(std::vector<std::string>& inputVec, std::string input) {
+	std::stringstream inputStream(input);
+	std::string str;
+
+	while(std::getline(inputStream, str, ' ')) {
+		inputVec.push_back(str);
+	}
+}
+
 void Server::receiveNewData( int fd ) {
 	char buff[ 1024 ]; // buffer to receive the data
 	memset(buff, 0, sizeof(buff)); // set the buffer to 0
@@ -146,6 +156,9 @@ void Server::receiveNewData( int fd ) {
 		}
 		buff[ bytes ] = '\0';
 		std::cout << "client : " << fd << " data : " << client_data->getInputBuffer() << std::endl;
+
+		std::vector<std::string> inputVec;
+		splitInput(inputVec, client_data->getInputBuffer());
 		// here is for the parsing of the data
 		client_data->clearInputBuffer();
 	}
