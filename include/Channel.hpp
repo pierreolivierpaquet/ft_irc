41aaf15c6a67db89e460eec72f39d61ab00d3dcb
Pierre Olivier Paquet <pierreolivierpaquet@hotmail.com>
2024-03-29 11:12:29 -0400

    ADDED [ Channel _mode + functions to set modes + retrieve modes { ex: isMode( DEFAULT_MODE | TOPIC_MODE | KEY_MODE ) } ]

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -4,10 +4,24 @@
 /*                                                                            */
 /******************************************************************************/
 
-#ifndef		__CHANNEL_H__
-# define	__CHANNEL_H__
+#ifndef		__CHANNEL_H
+# define	__CHANNEL_H
 
-# include "main.hpp"
+# include	"main.hpp"
+
+///	------------------------------------------------------------------- @section MACRO.S
+
+# ifndef	DEFAULT_MODE
+#  define	DEFAULT_MODE	0b00000001
+# endif	/*	DEFAULT_MODE	*/
+
+# define	INVITE_MODE		0b00000010
+# define	TOPIC_MODE		0b00000100
+# define	KEY_MODE		0b00001000
+# define	OPERATOR_MODE	0b00010000
+# define	LIMIT_MODE		0b00100000
+
+///	------------------------------------------------------------------- @section CLASS.ES
 
 class	Channel {
 
@@ -19,11 +33,15 @@ class	Channel {
 		std::string getName( void );
 		void addClient( Clients client );
 
+		void		setMode( u_int16_t mask );
+		u_int16_t	getMode( void ) const;
+		bool		isMode( u_int16_t mode ) const;
+
 	private:
 
 		std::map<int, Clients> _clientList;		//	MAP Container: <key>[ Clients->_fd ]:<value>[ Clients instance ]
 		std::string _name;
-		// u_int16_t	_mode; // Bitfield to monitor channel's mode(s).
+		u_int16_t	_mode; // Bitfield to monitor channel's mode(s).
 };	/*	Channel	*/
 
-#endif // __CHANNEL_H__
+#endif // __CHANNEL_H
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -23,6 +23,20 @@ std::string Channel::getName( void ) {
 	return (_name);
 }
 
+void	Channel::setMode( u_int16_t mask ) {
+	this->_mode |= mask;
+	return ;
+}
+
+u_int16_t	Channel::getMode( void ) const {
+	return ( this->_mode );
+}
+
+bool	Channel::isMode( u_int16_t mode ) const {
+	u_int16_t	status = this->getMode() & mode;
+	return ( status == mode );
+}
+
 Channel::Channel( std::string name): _name(name) {}
 
 Channel::~Channel( void ) {}
