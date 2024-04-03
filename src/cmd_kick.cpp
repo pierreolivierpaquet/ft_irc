ac213abb81bc44631a38c8dd0426df5541fabfd5
Eliott Vallee dore <evalldore@gmail.com>
2024-04-03 15:13:36 -0400

    kicking reserved for operators/

--- src/cmd_kick.cpp
+++ src/cmd_kick.cpp
@@ -6,6 +6,36 @@
 
 #include	"main.hpp"
 
-// void	kick( Server &ircserv, Clients &client, std::vector< std::string > param ) {
-// 	return ;
-// }
+void channelKick( Channel *channel, Clients &client, std::vector< std::string > param ) {
+	std::map<int, Clients>::iterator it;
+	std::string str = getSendID(client) + " KICK " + param.at(1) + ' ' + param.at(2) + ' ' + " :" + client.getNickName() + "\r\n";
+
+	for (it = channel->getClientList().begin(); it != channel->getClientList().end(); ++it) {
+		send(it->first, str.c_str(), str.length(), 0);
+	}
+}
+
+void	kick( Server &ircserv, Clients &client, std::vector< std::string > param ) {
+	if (param.size() < 3) {
+		std::string temp(":127.0.0.1 " + client.getPort() + " " + std::to_string( ERR_NEEDMOREPARAMS ) + " " + client.getNickName() + " :Need more parameters\r\n");
+		send( client.getFd(), temp.c_str(), temp.size(), 0 );
+		return ;
+	}
+
+	Channel *targetChannel;
+
+	if (!ircserv.getChannel(param.at(1), &targetChannel)) {
+		std::string temp(":127.0.0.1 " + client.getPort() + " " + std::to_string( ERR_NOSUCHCHANNEL ) + " " + client.getNickName() + " :No such channel\r\n");
+		send( client.getFd(), temp.c_str(), temp.size(), 0 );
+		return ;
+	}
+
+	if (targetChannel->findClient(client.getNickName()) != -1) {
+		if ( targetChannel->findOperator(client.getFd()) == targetChannel->getOper().end()) {
+			std::string temp(":127.0.0.1 " + client.getPort() + " " + std::to_string( ERR_CHANOPRIVSNEEDED ) + " " + client.getNickName() + " :You're not channel operator\r\n");
+			send( client.getFd(), temp.c_str(), temp.size(), 0 );
+			return;
+		}
+		channelKick(targetChannel, client, param);
+	}
+}
